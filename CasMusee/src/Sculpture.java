e81b9a160d59e9795f1eaa8978f5bdf34bc1d2f3
Munderstand <44328530+Munderstand101@users.noreply.github.com>
2021-10-07 22:29:40 +0200

    Ajout de la classe Sculpture herite de la classe Oeuvre

---
+++ CasMusee/src/Sculpture.java
@@ -0,0 +1,21 @@
+public class Sculpture extends Oeuvre{
+    private String materiau;
+
+    public Sculpture(String ptitre, int pannee, String partiste, String materiau) {
+        super(ptitre, pannee, partiste);
+        this.materiau = materiau;
+    }
+
+    public void afficherCarac() {
+        super.afficheCarac();
+        System.out.println("Materiau : " + this.materiau);
+    }
+
+    public String getMateriau() {
+        return materiau;
+    }
+
+    public void setMateriau(String materiau) {
+        this.materiau = materiau;
+    }
+}
